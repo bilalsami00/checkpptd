59aeab9af460e471865f91b81e948e7e6b629c5e
Muhammad Bilal Sami <101085324+bilalsami00@users.noreply.github.com>
2025-06-13 15:26:48 +0500

    13june25 3:26pm

--- src/app/Signup/page.tsx
+++ src/app/Signup/page.tsx
@@ -330,7 +330,7 @@
 import { Suspense } from "react";
 import SignupClientPage from "./SignupClientPage";
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 export default function SignupPageWrapper() {
   return (
